bba51dd795e2016d88764beabb340b0b755adba1
Max Boettcher <mvboettcher@gmail.com>
2021-02-28 21:24:46 -0500

    updates HomeListingCard styling

--- src/components/HomeListingCard/HomeListingCard.component.jsx
+++ src/components/HomeListingCard/HomeListingCard.component.jsx
@@ -31,25 +31,31 @@ function HomeListingCard({ classes, listing }) {
 
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image={imageURL} title="house" />
-      <CardContent>
-        <Typography gutterBottom variant="h4" component="h2">
-          {homeName}
-        </Typography>
-        <Typography
-          gutterBottom
-          variant="body2"
-          color="textSecondary"
-          component="p"
-        >
-          Starting in the ${formatNum(startingPrice)}s
-        </Typography>
-        <Typography variant="body1" component="p">
-          {`${formatNum(
-            sqft
-          )} sq. ft ${beds} beds ${baths} baths ${sectionType}`}
-        </Typography>
-      </CardContent>
+      <CardActionArea className={classes.actionArea}>
+        <CardMedia
+          className={classes.media}
+          image={imageURL}
+          title={homeName}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h4" component="h2">
+            {homeName}
+          </Typography>
+          <Typography
+            gutterBottom
+            variant="body2"
+            color="textSecondary"
+            component="p"
+          >
+            Starting in the ${formatNum(startingPrice)}s
+          </Typography>
+          <Typography variant="body1" component="p">
+            {`${formatNum(
+              sqft
+            )} sq. ft ${beds} beds ${baths} baths ${sectionType}`}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
     </Card>
   )
 }
--- src/components/HomeListingCard/HomeListingCard.styles.js
+++ src/components/HomeListingCard/HomeListingCard.styles.js
@@ -2,9 +2,10 @@ export default function styles(theme) {
   return {
     root: {
       maxWidth: 420,
-      height: 400,
       borderRadius: 0,
-      cursor: 'pointer',
+    },
+    actionArea: {
+      paddingBottom: 20,
     },
     media: {
       height: 240,
